793cf4b2b0e212ef8aa846d1db99e6f83321032f
javilonas <admin@lonasdigital.com>
2014-05-23 14:33:27 +0200

    Revert "ext4: fix memory leak in xattr (test)"
    This reverts commit 0ac0c8277afb8e22718385643ad3aeb8f187b122.

--- fs/ext4/xattr.c
+++ fs/ext4/xattr.c
@@ -1213,8 +1213,6 @@ retry:
 			if (!tried_min_extra_isize && s_min_extra_isize) {
 				tried_min_extra_isize++;
 				new_extra_isize = s_min_extra_isize;
-				kfree(is); is = NULL;
-				free(bs); bs = NULL;
 				brelse(bh);
 				goto retry;
 			}
