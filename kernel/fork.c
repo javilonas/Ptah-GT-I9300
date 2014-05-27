5a46f815f0f4b417bea32aa1ae0b6cb9c7c98c15
javilonas <admin@lonasdigital.com>
2014-05-27 15:50:48 +0200

    Revert "mm, mempolicy: remove per-process flag"
    This reverts commit 57e65d20e7e07b713fee6cdab60308a5233fdc2d.

--- kernel/fork.c
+++ kernel/fork.c
@@ -1210,6 +1210,7 @@ static struct task_struct *copy_process(unsigned long clone_flags,
  		p->mempolicy = NULL;
  		goto bad_fork_cleanup_cgroup;
  	}
+	mpol_fix_fork_child_flag(p);
 #endif
 #ifdef CONFIG_TRACE_IRQFLAGS
 	p->irq_events = 0;
