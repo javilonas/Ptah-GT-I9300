9fabd58683bb1ca35e8515b12343d5194f6917fa
javilonas <admin@lonasdigital.com>
2014-05-31 21:50:04 +0200

    Revert "mach-midas: disminuidos los límites"
    This reverts commit d1207155a6df821eca08266bb9f3adc04653a7b1.

--- arch/arm/mach-exynos/mach-midas.c
+++ arch/arm/mach-exynos/mach-midas.c
@@ -3817,19 +3817,19 @@ static struct platform_device *midas_devices[] __initdata = {
 /* below temperature base on the celcius degree */
 struct s5p_platform_tmu midas_tmu_data __initdata = {
 	.ts = {
-		.stop_1st_throttle  = 81,
-		.start_1st_throttle = 86,
-		.stop_2nd_throttle  = 91,
+		.stop_1st_throttle  = 85,
+		.start_1st_throttle = 90,
+		.stop_2nd_throttle  = 95,
 		.start_2nd_throttle = 103,
 		.start_tripping	    = 110, /* temp to do tripping */
 		.start_emergency    = 120, /* To protect chip,forcely kernel panic */
-		.stop_mem_throttle  = 82,
-		.start_mem_throttle = 87,
+		.stop_mem_throttle  = 83,
+		.start_mem_throttle = 88,
 		.stop_tc  = 13,
 		.start_tc = 10,
 	},
 	.cpufreq = {
-		.limit_1st_throttle  = 1000000, /* 1.000MHz in KHz order */
+		.limit_1st_throttle  = 1200000, /* 1.200MHz in KHz order */
 		.limit_2nd_throttle  = 200000, /* 200MHz in KHz order */
 	},
 	.temp_compensate = {
