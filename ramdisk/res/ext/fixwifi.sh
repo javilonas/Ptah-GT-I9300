6d02932874b340d9a5e88ede97c07b44d3c70fde
javilonas <admin@lonasdigital.com>
2014-06-04 12:28:02 +0200

    Update Files and  config

--- ramdisk/res/ext/fixwifi.sh
+++ ramdisk/res/ext/fixwifi.sh
@@ -3,12 +3,13 @@
 #
 
 /sbin/busybox rm /data/.cid.info
+/sbin/busybox sleep 2
 /sbin/busybox printf "Samsung | murata | semco | semcosh | semcove" > /data/.cid.info
 /sbin/busybox chown system /data/.cid.info
 /sbin/busybox chown -R 1000 /data/.cid.info
 /sbin/busybox chgrp wifi /data/.cid.info
 /sbin/busybox chgrp -R 1010 /data/.cid.info
-/sbin/busybox chmod 0666 /data/.cid.info
-/sbin/busybox chmod -R 0666 /data/.cid.info
+/sbin/busybox chmod 0660 /data/.cid.info
+/sbin/busybox chmod -R 0660 /data/.cid.info
 /sbin/busybox sync
 
